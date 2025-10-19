7988b4a8e340273941f0aad36b885e27f2afc9b5
𝐴𝑏𝑢 𝐽𝑎𝑚𝑎𝑙 | CyberPhantom <mccarthy29mp@gmail.com>
2025-10-19 23:01:01 +0300

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -2,9 +2,17 @@
 { pkgs }: {
   deps = [
     pkgs.python3
-    pkgs.chromium
-    pkgs.chromedriver
     pkgs.python3Packages.pip
     pkgs.python3Packages.virtualenv
+    pkgs.chromium
+    pkgs.chromedriver
+    pkgs.git
+    pkgs.psutil
+    pkgs.cacert
   ];
+  
+  env = {
+    PYTHONPATH = "./";
+    REPLIT_ENVIRONMENT = "production";
+  };
 }
